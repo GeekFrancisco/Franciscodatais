14d6334241af5a8c766314d1ad9fd2a3ecc5a3a6
Francisco <kargel.francisco@gmail.com>
2026-02-23 06:37:45 -0300

    Atualiza instrução de trabalho

--- archive/Backlog_SPN_Report_06.py
+++ archive/Backlog_SPN_Report_06.py
@@ -37,7 +37,7 @@ def gerar_pdf(df, colunas_desejadas, nome_arquivo, data_inicio, data_fim):
 
     # Título
     pdf.set_font("Arial", 'B', size=16)
-    pdf.cell(0, 10, txt=f"Relatório de Backlog - Semana de {data_inicio} até {data_fim} - ITI", ln=True, align='C')
+    pdf.cell(0, 10, txt=f"Relatório de Backlog - Semana de {data_inicio} até {data_fim} - SPN", ln=True, align='C')
     pdf.ln(5)
 
     # Cabeçalho
@@ -95,8 +95,8 @@ def gerar_pdf(df, colunas_desejadas, nome_arquivo, data_inicio, data_fim):
 
 # Caminho para o arquivo Excel (ajustado para data/base)
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'base'))
-caminho_arquivo = os.path.join(base_dir, 'Backlog_3.xlsx')
-aba = 'ITI'
+caminho_arquivo = os.path.join(base_dir, 'Backlog_6.xlsx')
+aba = 'SPN'
 
 # Lê a planilha
 df = ler_arquivo_excel(caminho_arquivo, aba)
@@ -142,7 +142,7 @@ if not os.path.exists(diretorio_relatorio):
     os.makedirs(diretorio_relatorio)
 
 # Define o caminho completo para o arquivo PDF
-caminho_arquivo_pdf = os.path.join(diretorio_relatorio, 'Report_ITI_Backlog_Semana_03_2026.pdf')
+caminho_arquivo_pdf = os.path.join(diretorio_relatorio, 'Report_SPN_Backlog_Semana_06_2026.pdf')
 
 # Gera o PDF com formatação em vermelho para "Pendentes"
 gerar_pdf(df_filtrado, colunas_desejadas + ['Tempo (Meses)'], caminho_arquivo_pdf, data_inicio_formatada, data_fim_formatada)
